c0ea2985a0901f92e85ed1ecb7c1710285ff4189
Jeevanantham <iamjeeva.aj@gmail.com>
2025-08-04 22:15:24 +0530

    Create 2025-08-04_MultiplicationTable.java

---
+++ 2025-08-04_MultiplicationTable.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public String[] multiplicationTable(int n) {
+        String[] table = new String[10];
+        for (int i = 1; i <= 10; i++) {
+            table[i - 1] = n + " x " + i + " = " + (n * i);
+        }
+        return table;
+    }
+}
